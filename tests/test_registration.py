76381edeb485b258588de1dd87a873626662410c
z3kanya <romanovpain@mail.ru>
2026-05-14 23:18:29 +0600

    добавлены автотесты для регистрации - tc004 и tc005

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -6,7 +6,7 @@ class TestRegistration:
     """Тесты регистрации"""
     REGISTER_URL = '/api/core/register/'
 
-    def test_successful_registration(self, api_client):
+    def test_tc001_successful_registration(self, api_client):
         """Успешная регистрация пользователя."""
         data = {
             'username': 'newuser',
@@ -25,7 +25,7 @@ class TestRegistration:
         assert 'token' in json_data
 
     
-    def test_existing_user(self, api_client, test_user):
+    def test_tc002_existing_user(self, api_client, test_user):
         """Регистрация существующего пользователя"""
         data = {
             'username': test_user.username,
@@ -37,13 +37,14 @@ class TestRegistration:
         response = api_client.post(self.REGISTER_URL, data, format='json')
         assert response.status_code == 400
         assert 'username' in response.json()['errors']
+        assert 'email' in response.json()['errors']
     
 
-    def test_password_mismatch(self, api_client):
+    def test_tc003_password_mismatch(self, api_client):
         """Несовпадение паролей"""
         data = {
             'username': 'user',
-            'email': 'example@example',
+            'email': 'example@example.com',
             'password': 'TestPass123',
             'password2': 'WrongPass123',
         }
@@ -52,4 +53,31 @@ class TestRegistration:
         assert response.status_code == 400
         assert 'password' in response.json()['errors']
     
-    # Дописать...
+    
+    
+    def test_tc004_invalid_email_no_tld(self, api_client):
+        """Почта без домена первого уровня"""
+        data = {
+            'username': 'user',
+            'email': 'user@example',
+            'password': 'TestPass123',
+            'password2': 'TestPass123',
+        }
+
+        response = api_client.post(self.REGISTER_URL, data, format='json')
+        assert response.status_code == 400
+        assert 'email' in response.json()['errors']
+
+
+    def test_tc005_invalid_email_no_at_symbol(self, api_client):
+        """Почта без символа @"""
+        data = {
+            'username': 'user',
+            'email': 'userexample.com',
+            'password': 'TestPass123',
+            'password2': 'TestPass123',
+        }
+
+        response = api_client.post(self.REGISTER_URL, data, format='json')
+        assert response.status_code == 400
+        assert 'email' in response.json()['errors']
